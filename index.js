f0993d554b88e90f91a9f351e0a0986399858650
Jacqueline Watson-Sinclair <faycello75@gmail.com>
2023-07-05 02:41:26 -0400

    defined functions using multiple methods

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+    
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"));
+
+  /*
+  function wrapAdjective(emphatic = "*") {
+    let trait1 = wrapAdjective("*");
+    let emphatic = trait1
+    return `You are *${emphatic}*`
+    /*return function (trait = "special") {
+      return `You are *${trait}*!`;
+    };
+       
+  }
+  console.log((wrapAdjective)("*a hard worker*")("||a dedicated programmer||"));
+*/
+
+  /*
+  function wrapAdjective() {
+  
+  }
+*/
+
+
+function wrapAdjective(highlight = "*") {
+  return function (uplift = "special") {
+    return `You are ${highlight}${uplift}${highlight}!`
+  };
+}
+console.log(wrapAdjective()("a hard worker"), ("||a dedicated programmer||"));
